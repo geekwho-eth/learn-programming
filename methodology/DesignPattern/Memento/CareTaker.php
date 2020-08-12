7556274c24acac3e01aac89bac0951a9f03dbeb4
GeekWho <caijiamx@gmail.com>
2020-08-13 07:25:48 +0800

    feat: 新增备忘录模式

---
+++ methodology/DesignPattern/Memento/CareTaker.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DesignPattern\Memento;
+
+class CareTaker
+{
+   private $mementoList = [];
+
+   public function add($state){
+      $this->mementoList[] = ($state);
+   }
+
+   public function get($index){
+      return $this->mementoList[$index];
+   }
+}
