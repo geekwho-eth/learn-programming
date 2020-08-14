ffb8d50a1f009a1cc382093056766f22a221b490
GeekWho <caijiamx@gmail.com>
2020-08-14 23:35:02 +0800

    feat: 新增MVC模式

---
+++ practice/tests/unittest/DesignPattern/J2ee/MvcTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Mvc模式 测试用例
+ */
+class MvcTest extends \PHPUnit\Framework\TestCase
+{
+    public function testMvc()
+    {
+        $this->expectOutputString("Student: Name: RobertRoll No: 10Student: Name: JohnRoll No: 10");
+        $mvc = new \DesignPattern\J2eePattern\Mvc\MVCPatternDemo();
+        $mvc->run();
+    }
+}
