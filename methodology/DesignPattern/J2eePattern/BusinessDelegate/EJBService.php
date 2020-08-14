36247bb46e1aab91bfe6f8e4f6da77105fd5e0e7
GeekWho <caijiamx@gmail.com>
2020-08-14 23:57:41 +0800

    feat: 新增业务代表模式

---
+++ methodology/DesignPattern/J2eePattern/BusinessDelegate/EJBService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DesignPattern\J2eePattern\BusinessDelegate;
+
+class EJBService implements IBusinessService
+{
+    public function doProcessing(){
+        echo "Processing task by invoking EJB Service";
+    }
+}
---
+++ practice/tests/unittest/DesignPattern/J2ee/BusinessDelegateTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 业务代表模式 测试用例
+ */
+class BusinessDelegateTest extends \PHPUnit\Framework\TestCase
+{
+    public function testBusinessDelegate()
+    {
+        $this->expectOutputString("Processing task by invoking EJB ServiceProcessing task by invoking JMS Service");
+        $businessDelegate = new \DesignPattern\J2eePattern\BusinessDelegate\BusinessDelegatePatternDemo();
+        $businessDelegate->run();
+    }
+}
